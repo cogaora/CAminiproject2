1763c0cf7ac7292d2d37500c68cedb3bd634bacb
Sacha Lévy <sacha.levy@mail.mcgill.ca>
2022-02-21 21:00:41 -0500

    add tcl file to compile and run the memory test bench

---
+++ memory_tb.tcl
@@ -0,0 +1,26 @@
+proc AddWaves {} {
+	;#Add waves we're interested in to the Wave window
+    add wave -position end sim:/cache_tb/clk
+    add wave -position end sim:/cache_tb/s_reset
+}
+
+vlib work
+
+;# Compile components if any
+vcom memory.vhd
+vcom memory_tb.vhd
+
+;#vcom cache.vhd
+;#vcom cache_tb.vhd
+
+;# Start simulation
+vsim -t 100ps memory_tb
+
+;# Generate a clock with 1ns period
+force -deposit clk 0 0 ns, 1 0.5 ns -repeat 1 ns
+
+;# Add the waves
+;#AddWaves
+
+;# Run for 50 ns
+run 100ns
